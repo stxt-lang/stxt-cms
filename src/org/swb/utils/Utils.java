b727daca34d7628337a2b1d40f0c4f33dd92d318
Joan Costa Mombiela <joan.costa.mombiela@gmail.com>
2026-07-18 19:28:19 +0200

    M+

--- src/org/swb/utils/Utils.java
+++ src/org/swb/utils/Utils.java
@@ -1,9 +1,22 @@
 package org.swb.utils;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.apache.commons.lang.StringEscapeUtils;
 
 public class Utils
 {
+	/** Marca de generación para cache-busting de assets (CSS/JS).
+	 *  Se calcula una sola vez por ejecución, así todas las páginas
+	 *  del build comparten el mismo valor. */
+	private static final String BUILD_VERSION = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+
+	public String version()
+	{
+		return BUILD_VERSION;
+	}
+
 	public String escapeHtml(String text)
 	{
 		return StringEscapeUtils.escapeHtml(text);
